3f08da2b2841bc1aac3c09efc0248c62584447d5
Abah Dominic <121255352+dommmy200@users.noreply.github.com>
2024-10-18 11:19:16 +0100

    added content to utils

---
+++ src/js/utilities.js
@@ -0,0 +1,34 @@
+// wrapper for querySelector...returns matching element
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+// retrieve data from localStorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+// save data to localStorage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// get URL parameters
+export function getParams(param) {
+  const queryString = window.location.search; // Get the query string from the URL
+  const urlParams = new URLSearchParams(queryString); // Create a URLSearchParams object
+  return urlParams.get(param); // Return the parameter value
+}
+
+const apiKey = '5da6d7f6efmsh009188dc742150fp1b8341jsn543dea9be0a9'; // Replace with your actual key
+
+fetch(`https://booking-com.p.rapidapi.com/v1/hotels/facilities?hotel_id=1676161&locale=en-gb`, {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': apiKey,
+    'x-rapidapi-host': 'booking-com.p.rapidapi.com'
+  }
+})
+.then(response => response.json()) // Parse JSON response
+.then(data => console.log(data))
+.catch(error => console.error('Error fetching data:', error));
