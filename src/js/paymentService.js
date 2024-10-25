b5621ccf0508189fd539e155ae988ad8609c83a2
Abah Dominic <121255352+dommmy200@users.noreply.github.com>
2024-10-25 15:15:29 +0100

    Modified Payment

--- src/js/paymentService.js
+++ src/js/paymentService.js
@@ -1,9 +1,10 @@
 function makePayment() {
-  document
-    .getElementById("payment")
-    .addEventListener("DOMContentLoader", (e) => {
+  const payment = document.getElementById("payment");
+  payment.addEventListener("DOMContentLoaded", (e) => {
       e.preventDefault();
       console.log("Making Payment!");
+      return `
+      <p class="xyz">You should be redirected to make payment!</p>`
       //Get user information from localStorage
       //Get external payment API
       //Request for card information...bring up a form to enter user card details
