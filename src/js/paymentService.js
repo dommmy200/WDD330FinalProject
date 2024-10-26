3f84056a355fb83e79ad7e0bcc35a7ab51a3cb0d
Abah Dominic <121255352+dommmy200@users.noreply.github.com>
2024-10-26 14:19:25 +0100

    Constructed transaction form

--- src/js/paymentService.js
+++ src/js/paymentService.js
@@ -42,14 +42,30 @@ function billingInfo() {
   const toPayment = document.getElementById('to-home');
   
   toPayment.addEventListener('click', () => {
-    alert('Thank you for patronizing!');
-    window.location.href = '../search-form/search.html';
+    window.location.href = '../booking-confirmation/payment.html';
   });
 
   toHomePage.addEventListener('click', () => {
     alert('We guarantee an availability\nof more choices for you.')
-    window.location.href = '../search-result/results.html';
+    window.location.href = '../search-form/search.html';
   });
 }
+function transactionForm() {
+  const payment = document.getElementById('payment')
+  const form = `
+    <form id="transaction">Payment Transaction
+      <label><input type="text" id="fname" name="fname">First Name: </label>
+      <label><input type="text" id="lname" name="lname">Last Name: </label>
+      <label><input type="text" id="credit-card" name="credit-card" placeholder="e.g. 1213-0087-9086-4563">Credit Card: </label>
+      <label><input type="date" id="expiration" name="expiration" placeholder="e.g. 06/24">Expiration Date:</label>
+      <label><textarea id="address" name="address" cols="30" rows="5" placeholder="Write billing address">Billing Address:</label>
+    </form>
+  `;
+  payment.innerHTML = form;
+  document.getElementById('transaction').addEventListener('submit', () => {
+
+  });
+}
+transactionForm();
 billingInfo();
 makePayment();
