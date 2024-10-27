6e378d445582c0fa8d78d0570d1abfeed8467dc9
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-10-27 19:40:50 +0100

    More changes to transactions

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -5,9 +5,28 @@ export function transactionForm() {
         const transaction = document.getElementById('transaction')
         const cardRendered = renderCard();
         transaction.innerHTML = cardRendered;
-
-        document.getElementById('submit').addEventListener('submit', () => {
-
-        });
+        // From database:
+        // Get card number, cvv, type, savings, background img, fname, lname,
+        // username
+        try {
+            // const profileArray = [];
+            const fname = document.getElementById("firstname").value;
+            const lname = document.getElementById("surname").value;
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+        
+            const profileObj = {
+                fname: `${fname}`,
+                lname: `${lname}`,
+                username: `${username}`,
+                password: `${password}`,
+            };
+        
+            localStorage.setItem("userProfile", JSON.stringify(profileObj));
+            alert("Thank You!\n\nRedirecting to make payment.");
+            window.location.href = "../booking-confirmation/payment.html";
+        } catch (error) {
+            console.log("Error:", error);
+        }
     });
-  }
+}
