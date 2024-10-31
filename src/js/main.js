e4cfd9210d722f3d85b54102f117e11a2b32104e
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-10-31 16:36:36 +0100

    img 2 searchPage

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,18 @@ import { fetchMockarooData } from "./utils.mjs";
 
 console.log("Hello world!");
 
-// export async function fetchHotels() {
-//   try {
-//     const response = await fetch("http://localhost:3000/hotels"); // Backend API URL
-//     const hotels = await response.json();
-
-//     const hotelList = document.getElementById("hotel-list");
-
-//     // Populate the list with hotel data
-//     hotels.forEach((hotel) => {
-//       const listItem = document.createElement("li");
-//       listItem.textContent = `${hotel.hotel_name} - ${hotel.city}, $${hotel.price} per night`;
-//       hotelList.appendChild(listItem);
-//     });
-//   } catch (error) {
-//     console.error("Error fetching hotels:", error);
-//   }
-// }
+const backgroundElement = document.getElementById("backgroundElement");
+const mobileImage = "../public/images/searchPage-mobile.webp";
+const desktopImage = "../public/images/searchPage-desk.webp";
+// Function to swap background based on screen size
+function setBackground() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        // Mobile view
+        backgroundElement.style.backgroundImage = `url(${mobileImage})`;
+    } else {
+        // Desktop view
+        backgroundElement.style.backgroundImage = `url(${desktopImage})`;
+    }
+}
 fetchMockarooData();
-// initializeHomePage();
+setBackground();
