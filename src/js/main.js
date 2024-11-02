5339af52a6ad68f6781c56cee43ff3d1d9c4f9c4
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-11-02 11:21:12 +0100

    SearchForm(style=ok)

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,5 @@ import { fetchMockarooData } from "./utils.mjs";
 console.log("Hello world!");
 
 
-// Function to swap background based on screen size
-function setBackground() {
-    const backgroundElement = document.getElementById("backgroundElement");
-    const mobileImage = "/images/searchPage-mobile.webp";
-    const desktopImage = "/images/searchPage-desk.webp";
-    console.log("function call!");
-    if (window.matchMedia("(max-width: 768px)").matches) {
-        // Mobile view
-        backgroundElement.style.backgroundImage = `url(${mobileImage})`;
-    } else {
-        // Desktop view
-        backgroundElement.style.backgroundImage = `url(${desktopImage})`;
-    }
-}
+
 fetchMockarooData();
-setBackground();
-// Listen for changes in screen size
-window.addEventListener("resize", setBackground);
